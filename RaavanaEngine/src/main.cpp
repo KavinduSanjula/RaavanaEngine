121f240594f4c85d4ef4dcfd4aa8cc29297b98f2
kavindusanjula <kavindusanjula.1@gmail.com>
2022-06-24 15:41:41 +0530

    Configure Project

--- RaavanaEngine/src/main.cpp
+++ RaavanaEngine/src/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-	std::cout << "Raavana Engine" << std::endl;
-	std::cin.get();
+	GLFWwindow* window;
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(1280, 720, "Raavana Engine", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glewInit();
+
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
