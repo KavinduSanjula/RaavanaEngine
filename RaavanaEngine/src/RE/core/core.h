473693db612d448a373bd5c0c598a9a8b7b5481f
kavindusanjula <kavindusanjula.1@gmail.com>
2022-06-28 10:01:38 +0530

    ASSERT

--- RaavanaEngine/src/RE/core/core.h
+++ RaavanaEngine/src/RE/core/core.h
@@ -11,12 +11,16 @@
 #define LOG_WARN(x) spdlog::warn(x)
 #define LOG_CRITICAL(x) spdlog::critical(x)
 
+#define ASSERT(x) if(!(x)) __debugbreak()
+
 #else
 
 #define LOG_INF(x) 
 #define LOG_ERR(x) 
 #define LOG_WARN(x)
 #define LOG_CRITICAL(x)
+
+#define ASSERT(x) 
 
 #endif
 
--- RaavanaEngine/src/RE/core/repch.h
+++ RaavanaEngine/src/RE/core/repch.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <memory>
+#include <fstream>
 
 #include <string>
 #include <vector>
