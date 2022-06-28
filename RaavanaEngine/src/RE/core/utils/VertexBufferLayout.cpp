d63275b1efe843a9458f20ed6533abb6e4c53262
kavindusanjula <kavindusanjula.1@gmail.com>
2022-06-28 10:19:50 +0530

    Vertex buffer layout

---
+++ RaavanaEngine/src/RE/core/utils/VertexBufferLayout.cpp
@@ -0,0 +1,25 @@
+#include "repch.h"
+#include "VertexBufferLayout.h"
+
+namespace RE {
+	VertexBufferLayout::VertexBufferLayout()
+		:m_Stride(0)
+	{
+	}
+
+	VertexBufferLayout::~VertexBufferLayout()
+	{
+	}
+
+	template<typename T>
+	void VertexBufferLayout::Push(unsigned int count) {
+		static_assert(false);
+	}
+
+	template<>
+	void VertexBufferLayout::Push<float>(unsigned int count) {
+		VertexBufferLayoutElement element({ GL_FLOAT, count, GL_FALSE, sizeof(float) });
+		m_Elements.push_back(element);
+		m_Stride += count * sizeof(float);
+	}
+}
---
+++ RaavanaEngine/src/RE/core/utils/VertexBufferLayout.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "repch.h"
+#include "core.h"
+#include "renderer/opengl.h"
+
+namespace RE {
+
+	struct VertexBufferLayoutElement {
+		unsigned int type;
+		unsigned int count;
+		unsigned int normalized;
+		unsigned int size;
+	};
+
+	class VertexBufferLayout
+	{
+	private:
+		unsigned int m_Stride;
+		std::vector<VertexBufferLayoutElement> m_Elements;
+
+	public:
+		VertexBufferLayout();
+		~VertexBufferLayout();
+
+		inline std::vector<VertexBufferLayoutElement> GetElements() const { return m_Elements; }
+		inline unsigned int GetStride() const { return m_Stride; }
+
+		template<typename T>
+		void Push(unsigned int count);
+
+		template<>
+		void Push<float>(unsigned int count);
+
+	};
+}
